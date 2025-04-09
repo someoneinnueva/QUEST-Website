7e43869a3a86f6b9a7178f3a16cdcf2af0bf7b88
someoneinnueva <anameht@nuevaschool.org>
2025-04-08 21:11:48 -0700

    Update app.py

--- app.py
+++ app.py
@@ -15,13 +15,17 @@ final_cheat_sheet = pd.read_csv("cheat_sheet.csv")
 st.dataframe(final_cheat_sheet)
 
 # Display Graphs
-st.subheader("📈 AI-Generated Graphs")
+st.subheader("📈 Interactive Graph")
 fig2 = pio.read_json("fig2.json")
 st.plotly_chart(fig2)
 
 # Link to AI Report
 st.subheader("📜 AI Report")
 report_link = "https://docs.google.com/document/d/1RrfK8E6011Zv7yUY0A1m8onGyKgPZlFVSQH5RPzDv8k/edit?usp=sharing"
+st.markdown(f"[View Full Report on Google Docs]({report_link})")
+
+st.subheader("📜 Code Breakdown")
+report_link = "https://docs.google.com/presentation/d/1qizWfV1B1CJzF6_KA0qAXHtVL_DH6kXVqQgf41DnvzE"
 st.markdown(f"[View Full Report on Google Docs]({report_link})")
 
 # Footer
